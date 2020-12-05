5cfe6909ec539ea8641646422364dbac21c6f0a4
Natalia Ramirez <ntlc.ramirez@gmail.com>
2020-12-05 15:40:12 +1300

    almost final, need validation

--- js/script.js
+++ js/script.js
@@ -494,30 +494,25 @@ $('body').append(script);
 // Validation form
 // ==========================================================
 
-// function validateForm() {
-//   var x = document.forms("bookingForm").value;
-//   if (x == "") {
-//     alert("Missing Information!, all field are required");
-//     return false;
-//   }
-// }
 
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('button', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+// (function() {
+//   'use strict';
+//   window.addEventListener('load', function() {
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.getElementsByClassName('needs-validation');
+//     // Loop over them and prevent submission
+//     var validation = Array.prototype.filter.call(forms, function(form) {
+//       form.addEventListener('button', function(event) {
+//         if (form.checkValidity() === false) {
+//           event.preventDefault();
+//           event.stopPropagation();
+//         }
+//         form.classList.add('was-validated');
+//       }, false);
+//     });
+//   }, false);
+// })();
+
+
 
 }); //document.ready()
